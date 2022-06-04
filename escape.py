1f75c8e94529bd3a9c716f3f126de64fc478e22a
Jorik Tuinman <joriktuinman@hotmail.com>
2022-06-04 12:01:28 +0200

    .

--- escape.py
+++ escape.py
@@ -1,8 +1,11 @@
 import configparser
+import logging
 import os
 import sys
 
-from flask import Flask
+from flask import Flask, jsonify
+import time
+from escape_library import OutputPin, CaravanLoggingHandler
 
 rpi_complete_mode = False
 try:
@@ -12,6 +15,16 @@ try:
 except Exception:
     GPIO = False
 
+
+## Logger setup
+logger = logging.getLogger(__name__)
+logging.getLogger('werkzeug').setLevel(logging.ERROR)
+entriesHandler = CaravanLoggingHandler()
+logger.addHandler(logging.StreamHandler())
+logger.addHandler(entriesHandler)
+logger.setLevel(logging.INFO)
+
+
 app = Flask(__name__)
 
 ### Flask methods
@@ -19,6 +32,19 @@ app = Flask(__name__)
 def hello_world():
     return config['Escape']['debug']
 
+@app.route('/switch/<pinname>/<newstate>')
+def flask_set_switch(pinname, newstate):
+    pin = outputpins[pinname]
+    to_on = newstate == "1"
+    logger.info("Got web request to turn pin " + pin.name + (" ON" if to_on else " OFF"))
+    try:
+        if to_on:
+            pin.turn_on()
+        else:
+            pin.turn_off()
+    except Exception as e:
+        logger.error("Got exception trying to turn pin, " + str(e))
+    return jsonify(result="ok")
 
 ##Init stuff
 configfilename = "escape.conf"
@@ -30,5 +56,17 @@ try:
 except:
     print("Could not read " + configfile)
     sys.exit()
+
+
+##Init all pins
+fastenseatbeltlight = OutputPin(config.getint("Escape", "seatbeltlightpin"), "Fasten seatbelt light")
+time.sleep(0.5)
+magnet = OutputPin(config.getint("Escape", "magnetpin"), "Magnet")
+time.sleep(0.5)
+cabinet = OutputPin(config.getint("Escape", "cabinetpin"), "Cabinet")
+time.sleep(0.5)
+outputpins = {fastenseatbeltlight.name:fastenseatbeltlight, magnet.name:magnet, cabinet.name:cabinet}    
+
+
 
 app.run(debug=config.getboolean("Escape", "debug"),host="0.0.0.0",port=config.getint("Escape", "port"),threaded=True)
