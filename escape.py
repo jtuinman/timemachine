d1401b6128eee0952ef09229a8359fa1056807d4
Jorik Tuinman <joriktuinman@hotmail.com>
2022-06-04 11:06:01 +0200

    .

--- escape.py
+++ escape.py
@@ -17,13 +17,13 @@ app = Flask(__name__)
 ### Flask methods
 @app.route('/')
 def hello_world():
-    return 'Hello world'
+    return config.read(configfile)
 
 
 ##Init stuff
 configfilename = "escape.conf"
-configfile = (os.path.join(os.getcwd(), configfilename))
+configfile = (os.path.join(os.getcwd(), configfilename))    
 config = configparser.ConfigParser()
 
-app.run(debug=False,host="0.0.0.0",port=3000,threaded=True)
+app.run(debug=config.getboolean('DEFAULT', 'debug'),host="0.0.0.0",port=3000,threaded=True)
 ##app.run(debug=config.getboolean("Escape", "debug"),host="0.0.0.0",port=config.getint("Escape", "port"),threaded=True)
