3a3325487424b78b1c94a8b704c5448f4d4fd4c7
Jorik Tuinman <joriktuinman@hotmail.com>
2022-06-20 20:21:42 +0200

    .

--- subscriber.py
+++ subscriber.py
@@ -14,7 +14,7 @@ def on_message(client, userdata, message):
 
 signal.signal(signal.SIGINT, signal_handler)
 clientSubscribe = mqtt.Client("PlantReader")
-clientSubscribe.connect("localhost") 
+clientSubscribe.connect("192.168.178.30") 
 
 clientSubscribe.loop_start()
 clientSubscribe.unsubscribe("#")
