040d7b935024ba27a40d93a3aa462d30c64c52d9
Jorik Tuinman <joriktuinman@hotmail.com>
2022-06-22 20:06:35 +0200

    .

--- publisher.py
+++ publisher.py
@@ -4,40 +4,41 @@ import signal
 import sys
 import json
 
-def signal_handler(sig, frame):
-    clientSubscribe.loop_stop()
-    print("Exiting")
-    sys.exit(0)
+class Publish:
+    def signal_handler(sig, frame):
+        clientSubscribe.loop_stop()
+        print("Exiting")
+        sys.exit(0)
 
-def on_message(client, userdata, message):
-    print("received message: " ,str(message.payload.decode("utf-8")))
-
-
-signal.signal(signal.SIGINT, signal_handler)
-
-clientPublish = mqtt.Client("TestPublish")
-clientPublish.connect("192.168.178.30")
+    def on_message(client, userdata, message):
+        print("received message: " ,str(message.payload.decode("utf-8")))
 
 
-test = "lol"
-message = {
-        "timestamp": test
-        }
+    signal.signal(signal.SIGINT, signal_handler)
 
-jsonDump = json.dumps(message)
-topic="PLANTNET/LIGHT"
-clientPublish.publish(topic, jsonDump)
-print(jsonDump)
+    clientPublish = mqtt.Client("TestPublish")
+    clientPublish.connect("192.168.178.30")
 
-test = "Ha!"
-message = {
-        "timestamp": test
-        }
-
-jsonDump = json.dumps(message)
-topic="PLANTNET/LIGHT"
-clientPublish.publish(topic, jsonDump)
-print(jsonDump)
-
-time.sleep(10)
-clientPublish.loop_stop()
+#
+#test = "lol"
+#message = {
+#        "timestamp": test
+#        }
+#
+#jsonDump = json.dumps(message)
+#topic="PLANTNET/LIGHT"
+#clientPublish.publish(topic, jsonDump)
+#print(jsonDump)
+#
+#test = "Ha!"
+#message = {
+#        "timestamp": test
+#       }
+#
+#jsonDump = json.dumps(message)
+#topic="PLANTNET/LIGHT"
+#clientPublish.publish(topic, jsonDump)
+#print(jsonDump)
+#
+#time.sleep(10)
+#clientPublish.loop_stop()
